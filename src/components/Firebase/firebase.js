2e76195462a10941c924eb67110558c5da1b889f
Santosh Joseph <sjoseph@groupon.com>
2019-05-19 20:07:13 -0700

    removing config

--- src/components/Firebase/firebase.js
+++ src/components/Firebase/firebase.js
@@ -1,16 +1,7 @@
 import app from 'firebase/app';
 import 'firebase/auth';
 import 'firebase/database';
-
-const config = {
-  apiKey: "AIzaSyBa5hmLy1FIa1QmwEGKmWnIaWBYvrO-aBo",
-  authDomain: "blackjacktracker-37996.firebaseapp.com",
-  databaseURL: "https://blackjacktracker-37996.firebaseio.com",
-  projectId: "blackjacktracker-37996",
-  storageBucket: "blackjacktracker-37996.appspot.com",
-  messagingSenderId: "719500530307",
-  appId: "1:719500530307:web:81eb8fd402437697"
-};
+import config from '../../firebase-config';
 
 class Firebase {
   constructor() {
