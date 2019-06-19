16d628fcc12e12ace465b5ae0549abfefbd7ccac
Santosh Joseph <sjoseph@groupon.com>
2019-06-19 07:48:44 -0700

    formik, yup

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -2,6 +2,13 @@ import React from 'react';
 import { withAuthorization } from '../Session';
 import { Paper, Typography, Button, List, ListItem, ListItemText } from '@material-ui/core'
 import { withStyles } from '@material-ui/core/styles'
+import { Formik, Field, Form, ErrorMessage } from 'formik';
+// import FormControl from '@material-ui/core/FormControl';
+// import InputLabel from '@material-ui/core/InputLabel';
+// import FormHelperText from '@material-ui/core/FormHelperText';
+// import Input from '@material-ui/core/Input';
+import * as Yup from 'yup';
+import BookForm from './book-form';
 
 const styles = {
   root: {
@@ -10,6 +17,11 @@ const styles = {
     maxWidth: 400
   }
 }
+
+const validationSchema = Yup.object({
+  author: Yup.string("Author name").required("Name is required").min(5),
+  title: Yup.string("Book title").required("title is required")
+});
 
 class HomePage extends React.Component {
   constructor(props) {
@@ -25,7 +37,7 @@ class HomePage extends React.Component {
   async componentDidMount() {
     const snapshot = await this.props.firebase.books()
       .orderByKey()
-      .limitToFirst(this.pageSize+1)
+      .limitToFirst(this.pageSize + 1)
       .startAt('')
       .once('value');
     const values = snapshot.val();
@@ -38,28 +50,31 @@ class HomePage extends React.Component {
     })
   }
   add = async () => {
-    const bookNum = Math.round(Math.random()*1000);
-    this.props.firebase.books().push({title: `book${bookNum}`, author: 'S.A. Joseph'})
+    const bookNum = Math.round(Math.random() * 1000);
+    this.props.firebase.books().push({ title: `book${bookNum}`, author: 'S.A. Joseph' })
+  }
+  addNewBook = async (book) => {
+    return this.props.firebase.books().push(book)
   }
   prev = async () => {
     const snapshot = await this.props.firebase.books()
       .orderByKey()
-      .limitToFirst(this.pageSize+1)
-      .startAt(this.state.prevKeys[this.state.prevKeys.length-1])
+      .limitToFirst(this.pageSize + 1)
+      .startAt(this.state.prevKeys[this.state.prevKeys.length - 1])
       .once('value');
     const values = snapshot.val();
     const keys = Object.keys(values);
     this.setState({
       books: Object.values(values).slice(0, this.pageSize),
       keys: keys.slice(0, this.pageSize),
-      nextKey: keys[keys.length-1],
-      prevKeys: this.state.prevKeys.slice(0, this.state.prevKeys.length-1)
+      nextKey: keys[keys.length - 1],
+      prevKeys: this.state.prevKeys.slice(0, this.state.prevKeys.length - 1)
     });
   }
   next = async () => {
     const snapshot = await this.props.firebase.books()
       .orderByKey()
-      .limitToFirst(this.pageSize+1)
+      .limitToFirst(this.pageSize + 1)
       .startAt(this.state.nextKey)
       .once('value');
     const values = snapshot.val();
@@ -74,15 +89,16 @@ class HomePage extends React.Component {
   }
 
   render() {
-    const {classes} = this.props;
-    const books = this.state.books.map((x,i) => {
+    const { classes } = this.props;
+    const books = this.state.books.map((x, i) => {
       return (
         <ListItem key={this.state.keys[i]}>
-          <ListItemText primary={x.author}/>
-          <ListItemText primary={x.title}/>
+          <ListItemText primary={x.author} />
+          <ListItemText primary={x.title} />
         </ListItem>
       );
     });
+    const initialValues = { author: '', title: '' }
     return (
       <Paper className={classes.root}>
         <Typography variant='title' align='center' gutterBottom>
@@ -95,12 +111,25 @@ class HomePage extends React.Component {
           variant='contained'
           onClick={this.add}
         >
-          Add Book
+          Add Random Book
         </Button>
+        <br />
+        <Formik
+          initialValues={initialValues}
+          validationSchema={validationSchema}
+          onSubmit={async (values, actions) => {
+            console.log('formik submit', values)
+            const result = await this.addNewBook(values)
+            actions.setSubmitting(false)
+            console.log(result);
+          }}
+          render={props => <BookForm {...props} />}
+        />
+
         <List>
           {books}
         </List>
-        {<PrimaryButton disabled={this.state.prevKeys.length === 0} onClick={this.prev} text="Prev" color='secondary'/>}
+        {<PrimaryButton disabled={this.state.prevKeys.length === 0} onClick={this.prev} text="Prev" color='secondary' />}
         {<PrimaryButton disabled={!this.state.nextKey} onClick={this.next} text="Next" color='secondary' />}
       </Paper>
     );
